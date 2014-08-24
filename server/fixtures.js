ceaa6a227e726f5025c985cb71acc5c54abd6988
Matthew Vasseur <mattvasseur@gmail.com>
2014-08-24 17:14:50 -0400

    slight adjustments

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,1 +1,9 @@
+if (Posts.find().count() === 0) {
+  Posts.insert({
+    url: 'Question?',
+    have: 'Send an email to',
+    want: 'Matthew Vasseur',
+    email: 'matthew.vasseur@yale.edu'
+  });
 
+}
